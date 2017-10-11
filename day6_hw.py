ee5fa02c14dc93ab3c6e8aa83482b171029a4657
kyuu57 <32593925+kyuu57@users.noreply.github.com>
2017-10-11 15:09:35 +0900

    Add files via upload

---
+++ day6_hw.py
@@ -0,0 +1,48 @@
+import turtle as t
+t.shape("turtle")
+
+t.color("blue")
+t.bgcolor("white")
+t.speed(0)
+t.backward(85)
+
+angle=89
+for x in range(300):
+    t.fd(80)
+    t.lt(angle)
+t.rt(50)
+t.fd(70)
+
+t.color("black")
+angle=89
+for x in range(300):
+    t.fd(80)
+    t.lt(angle)
+t.rt(65)
+t.fd(55)
+
+t.color("red")
+angle=89
+for x in range(300):
+    t.fd(80)
+    t.lt(angle)
+t.rt(245)
+t.fd(30)
+
+t.color("green")
+angle=89
+for x in range(300):
+    t.fd(80)
+    t.lt(angle)
+t.color("white")
+t.lt(45)
+t.fd(30)
+
+t.rt(110)
+t.fd(70)
+
+t.color("yellow")
+angle=89
+for x in range(300):
+    t.fd(80)
+    t.lt(angle)
